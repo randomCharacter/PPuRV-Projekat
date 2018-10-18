21230b5cd2c29c50b0e78b29eb16242273a480ef
Mario Perić <pericmario96@gmail.com>
2018-10-18 19:50:39 +0000

    Header za biblioteku

---
+++ libHeap/heap.c
@@ -0,0 +1,36 @@
+#include "heap.h"
+
+void heap_init(heap_t* heap, size_t elem_size, size_t elem_no, int (*cmp) (const void *, const void*))
+{
+
+}
+
+void* heap_get_min(heap_t* heap)
+{
+	return (void*) 0;
+}
+
+void heap_add(heap_t* heap, void* value)
+{
+
+}
+
+void heap_remove(heap_t* heap, void* value)
+{
+
+}
+
+void* heapify(heap_t* heap, void* values, size_t size)
+{
+	return (void*) 0;
+}
+
+void heap_print(heap_t* heap)
+{
+
+}
+
+void heap_destroy(heap_t *heap)
+{
+
+}
